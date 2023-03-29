ceebdc0ba342f80d97a6ca84749c9f8376a6fda1
Сергей Сычёв <lutyi2856@gmail.com>
2023-03-29 09:24:15 +0300

    добавляет маштабирование и эфекты

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+import {resetEffects} from './effects.js';
+import {resetScale} from './scale.js';
 import {isEscapeKey} from './util.js';
 
 const MAX_HASHTAG_COUNT = 5;
@@ -50,11 +52,8 @@ const onInputUpload = () => {
   formPopup.classList.remove('hidden');
   document.body.classList.add('modal-open');
   pristine.reset();
-<<<<<<< HEAD
   resetScale();
   resetEffects();
-=======
->>>>>>> 37bc8ae1668f3776b2a2cfdc41065b27653b0b22
 };
 
 const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
@@ -80,11 +79,7 @@ pristine.addValidator (
   HASHTAGS_ERROR_TEXT
 );
 
-const validateTextDescription = (value) => {
-  if(value.length < MAX_LENGTH_DESCRIPTION) {
-    return true;
-  }
-};
+const validateTextDescription = (value) => value.length <= MAX_LENGTH_DESCRIPTION;
 
 pristine.addValidator (
   inputTextDescription,
@@ -93,9 +88,7 @@ pristine.addValidator (
 );
 
 const onFormSubmit = (evt) => {
-  if(pristine.validate()) {
-    formAddImage.submit();
-  } else {
+  if (!pristine.validate()) {
     evt.preventDefault();
   }
 };
